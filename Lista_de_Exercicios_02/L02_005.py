d1f8865fa8762a7fc078ba1d36b6b6a3c4eb7f97
Ricardo Kléber <ricardokleber@ricardokleber.com.br>
2023-05-09 09:32:25 -0300

    Delete L02_005.py

--- Lista_de_Exercicios_02/L02_005.py
+++
@@ -1,7 +0,0 @@
-A = float(input("Digite o valor de A: "))
-B = float(input("Digite o valor B: "))
-C = float( input("Digite o valor de C: "))
-if A <(B+C) and B <(A+C) and C <(A+B):
-   print("ABC é um triângulo!")
-else:
-   print("ABC não é um triângulo!")
