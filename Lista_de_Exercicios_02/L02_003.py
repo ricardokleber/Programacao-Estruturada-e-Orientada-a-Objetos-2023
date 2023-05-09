26ee880ef2a04dae6306077cde9e12cdf19a6fd8
Ricardo Kléber <ricardokleber@ricardokleber.com.br>
2023-05-09 09:32:11 -0300

    Delete L02_003.py

--- Lista_de_Exercicios_02/L02_003.py
+++
@@ -1,8 +0,0 @@
-nota1= float(input('Digite a primeira nota: '))
-nota2= float(input('Digite a segunda nota: '))
-media=(nota1 + nota2)/2
-print("A média Aritmética de", nota1,"e",nota2,"é:",media)
-if media >= 7:
-   print("Aluno aprovado!")
-else:
-   print("Aluno reprovado!")
