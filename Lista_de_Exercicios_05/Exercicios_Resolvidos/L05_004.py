6c651ad8c3270cd1f4f264bf74f180ae4b713d6d
Ricardo Kléber <ricardokleber@ricardokleber.com.br>
2023-05-09 20:01:04 -0300

    Update L05_004.py

--- Lista_de_Exercicios_05/Exercicios_Resolvidos/L05_004.py
+++ Lista_de_Exercicios_05/Exercicios_Resolvidos/L05_004.py
@@ -1,11 +1,9 @@
 string_consulta = input("Digite uma string: ")
 string_consulta = string_consulta
 contador = 0
-
 for caracter in string_consulta:
     for caracter_teste in string_consulta:
         if caracter == caracter_teste:
             contador += 1
-            
     print(f"O caractere {caracter} apareceu {contador} vez(es) na strig")
     contador = 0
