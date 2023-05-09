7794ba7e3a32ce82e7739440c7af71bd3059ad9e
Ricardo Kléber <ricardokleber@ricardokleber.com.br>
2023-05-09 09:31:56 -0300

    Delete L02_001.py

--- Lista_de_Exercicios_02/L02_001.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input("Insira o Primeiro Número : "))
-n2 = int(input("Insira o Segundo Número : "))
-if n1 > n2 :
-   print("O maior número digitado foi:",n1)
-else:
-   print("O maior número digitado foi:",n2)
