a169ea0b662d7fd3ec1c1977e01271907858c247
Ricardo Kléber <ricardokleber@ricardokleber.com.br>
2023-05-08 21:33:47 -0300

    Add files via upload

---
+++ Lista_de_Exercicios_02/L02_001.py
@@ -0,0 +1,6 @@
+n1 = int(input("Insira o Primeiro Número : "))
+n2 = int(input("Insira o Segundo Número : "))
+if n1 > n2 :
+   print("O maior número digitado foi:",n1)
+else:
+   print("O maior número digitado foi:",n2)
